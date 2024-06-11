aace80a49245ddf2c9794bbdfacd138e5806e3d4
Hendrio Marques <hendriomm@gmail.com>
2024-06-10 20:14:19 -0400

    feat: sysfs sample, attr_show

--- smartlamp-kernel-module/install_driver.sh
+++ smartlamp-kernel-module/install_driver.sh
@@ -3,10 +3,18 @@ sudo rmmod serial.ko
 sudo rmmod serial_write.ko
 sudo rmmod smartlamp.ko
 sudo rmmod sysfs.ko
+
+sudo rmmod serial
+sudo rmmod sysfs
+
 sudo rmmod usbserial
 
 sudo rmmod cp210x
 make
+
 sudo rmmod $1.ko
 sudo insmod $1.ko
 lsmod | grep $1
+
+
+
